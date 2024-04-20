43d7800469c7d2ebfb3a4d88e511b2296236f7f7
danielk-19 <167109780+danielk-19@users.noreply.github.com>
2024-04-20 16:22:14 -0400

    Add files via upload

---
+++ LocBlairUpdaterApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import UIKit
+
+@main
+struct LocBlairUpdaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootViewControllerContainer()
+        }
+    }
+}
