0bbdff5eb517334a3b52455b6f167326edb0cdbf
danielk-19 <dketema34@gmail.com>
2024-06-03 19:11:37 -0400

    Connection

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -7,6 +7,11 @@ export default function Home() {
     const { available, handleAvailabilityChange } = useContext(AvailabilityContext);
     const { locationInfoData } = useContext(LocationInfoContext);
 
+    const redirectToAnotherSite = (message) => {
+        const siteUrl = 'https://sites.google.com/view/wwwfalconlibraryorg';
+        window.parent.postMessage(message, siteUrl);
+    };
+
     return (
         <div className="center">
             <h1 className="status-circle">Status<br/>{available}<br />{locationInfoData.currentLocation}<br />{locationInfoData.currentTime}</h1>
@@ -18,6 +23,7 @@ export default function Home() {
                     <button className="buttonDesign" onClick={() => handleAvailabilityChange("OUT")}>Out</button>
                 </Link>
             </div>
+            <button className="buttonDesign" onClick={() => redirectToAnotherSite(available)}>Update</button>
         </div>
     );
 }
