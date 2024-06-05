e002ed1aa0674f11b5cec046bf2f0cf2cf73afad
threepont <67600885+threepont@users.noreply.github.com>
2024-06-04 22:41:02 -0400

    Update form2.js
    adds testing option, meeting option

--- my-app/src/pages/form2.js
+++ my-app/src/pages/form2.js
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useContext, useState } from 'react';
 import { Link } from "react-router-dom";
+import { SettingsContext } from '../context/SettingsContext';
+import { LocationInfoContext } from '../context/LocationInfoContext';
 
-export default function form2() {
+export default function Form2() {
+  const { locationInfoData, updateLocationInfoData } = useContext(LocationInfoContext);
+  const { settingsData } = useContext(SettingsContext);
+
+  const [localLocationInfo, setLocalLocationInfo] = useState(locationInfoData);
+
+  const handleTimeChange = (event) => {
+  const newTime = event.target.value;
+  setLocalLocationInfo((prev) => ({ ...prev, currentTime: newTime }));
+  };
+
+  const handleLocationChange = (event) => {
+    const newLocation = event.target.value;
+    setLocalLocationInfo((prev) => ({ ...prev, currentLocation: newLocation }));
+  };
+
+  const handleSubmitForm2 = (e) => {
+    e.preventDefault();
+    updateLocationInfoData(localLocationInfo);
+  };
     return (
         <div>
+            <div className="button-container">
+            <button 
+              value="Testing: " 
+              onClick={handleLocationChange} className="buttonDesign"
+            >Testing</button>
+            
+            </div>
+            <div className="button-container">
+            <button 
+              value="Meeting: " 
+              onClick={handleLocationChange} className="buttonDesign"
+            >Meeting</button>
+            
+            </div>
+            
+            
+            <div>
+                <form onSubmit={handleSubmitForm2} className="button-container">
+                <label>
+                    Location:
+                    <input type="text" value={localLocationInfo.currentLocation} onChange={handleLocationChange} />
+                </label>
+                {localLocationInfo.currentLocation && (
+                <div>
+                    <label>
+                    (Optional) Available until:
+                    <input type="time" value={localLocationInfo.currentTime} onChange={handleTimeChange} />
+                    </label>
+                </div>
+                )}
+                <input type="submit" value="Apply" />
+            </form>
+            </div>
 
             <div className="container center">
                 <Link to="/">
