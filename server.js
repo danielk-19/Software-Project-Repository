f1fc9e38ea7012cd4e2b33d3bb08545cef8c6cb7
danielk-19 <dketema34@gmail.com>
2024-06-06 19:44:15 -0400

    deploy

--- server.js
+++ server.js
@@ -4,12 +4,43 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://cloudchasers:joshuaTang123%21@software-project.yrguefx.mongodb.net/software-project?retryWrites=true&w=majority&appName=software-project', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  ssl: true
+
+
+const app = express();
+ 
+// Middleware to redirect HTTP to HTTPS
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(`https://${req.hostname}${req.url}`);
+  }
+  next();
 });
+ 
+const corsOptions = {
+  origin: 'https://danielk-19.github.io', // Replace with your frontend's URL
+  optionsSuccessStatus: 200,
+};
+ 
+// Apply CORS middleware
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+ 
+// MongoDB connection
+const uri = process.env.MONGODB_URI || 'mongodb+srv://cloudchasers:joshuaTang123%21@software-project.yrguefx.mongodb.net/software-project?retryWrites=true&w=majority&appName=software-project';
+ 
+const connectWithRetry = () => {
+  console.log('MongoDB connection with retry');
+  mongoose.connect(uri, {
+    ssl: true
+  }).then(() => {
+    console.log('MongoDB is connected');
+  }).catch((err) => {
+    console.error('MongoDB connection unsuccessful, retry after 5 seconds.', err);
+    setTimeout(connectWithRetry, 5000);
+  });
+};
+ 
+connectWithRetry();
 
 const dataSchema = new mongoose.Schema({
   data: String,
@@ -18,14 +49,6 @@ const dataSchema = new mongoose.Schema({
 
 const Data = mongoose.model('Data', dataSchema);
 
-const app = express();
-
-// Configure CORS options 
-const corsOptions = { origin: 'https://danielk-19.github.io',  optionsSuccessStatus: 200, }; 
-// Apply CORS middleware 
-app.use(cors(corsOptions)); 
-// Enable preflight requests for all routes 
-app.options('*', cors(corsOptions));
 app.use(bodyParser.json());
 
 // API endpoint to receive data
